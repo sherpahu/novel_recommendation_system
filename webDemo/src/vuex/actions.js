d2935fc2eef38e88cad140a9e3832955fe996820
Your Name <you@example.com>
2020-12-30 21:19:33 +0800

    update code

--- webDemo/src/vuex/actions.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by kalen on 10/14/16.
- */
-
-export const searchNovels = ({ dispatch }, novel_name) => {
-  dispatch('SEARCH_NOVELS', novel_name)
-};
--- webDemo/src/vuex/getters.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by kalen on 10/14/16.
- */
-
-export function getNovels (state) {
-  return state.novels
-}
